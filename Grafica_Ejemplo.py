9242a617227bcfd73c21acbda74522baa9fb74bc
DavidRaveloG <81648619+DavidRaveloG@users.noreply.github.com>
2021-04-18 16:23:29 -0500

    Add files via upload

---
+++ Grafica_Ejemplo.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+x=np.linspace(0,1,2)
+y1=0*x
+y2=0*x
+plt.plot(x,y2,label='Lapices')
+plt.plot(x*0,x,label='Gomas')
+plt.plot(x*0,x,label='Carboncillos')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('max z=4x+6y')
+plt.legend(loc=1)
+plt.scatter(1,0)
+plt.show()
