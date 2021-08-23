bdff53d7724f20bd5171c10edcf013128c4a1180
DavidRaveloG <81648619+DavidRaveloG@users.noreply.github.com>
2021-08-23 11:51:28 -0500

    Add files via upload

--- Combinaciones.py
+++ Combinaciones.py
@@ -3,21 +3,39 @@ def variables(x1,x2,x3):
     return (10000*x1)+(6000*x2)+(4000*x3)<=10000
     
 def F_objetivo(x1,x2,x3): 
-    return (11000*x1)+(9000*x2)+(10000*x3)
+    return (11000*x1)+(9000*x2)+(1000*x3)
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
 
 if(variables(1,1,1)==True):
+    a=F_objetivo(1,1,1)
     print(str(F_objetivo)(1,1,1)+' con x1=1,x2=1,x3=1')
 if(variables(1,1,0)==True):
+    b=F_objetivo(1,1,0)
     print(str(F_objetivo(1,1,0))+' con x1=1,x2=1,x3=0')
 if(variables(1,0,1)==True):
+    c=F_objetivo(1,0,1)
     print(str(F_objetivo(1,0,1))+' con x1=1,x2=0,x3=1')
 if(variables(0,1,1)==True):
+    d=F_objetivo(0,1,1)
     print(str(F_objetivo(0,1,1))+' con x1=0,x2=1,x3=1')
 if(variables(1,0,0)==True):
+    e=F_objetivo(1,0,0)
     print(str(F_objetivo(1,0,0))+' con x1=1,x2=0,x3=0')
 if(variables(0,1,0)==True):
+    f=F_objetivo(0,1,0)
     print(str(F_objetivo(0,1,0))+' con x1=0,x2=1,x3=0')
 if(variables(0,0,1)==True):
+    g=F_objetivo(0,0,1)
     print(str(F_objetivo(0,0,1))+' con x1=0,x2=0,x3=1')
 if(variables(0,0,0)==True):
+    h=F_objetivo(0,0,0)
     print(str(F_objetivo(0,0,0))+' con x1=0,x2=0,x3=0')    
+maximo=[a,b,c,d,e,f,g,h]   
+print('El maximo es: '+str(max(maximo)))
